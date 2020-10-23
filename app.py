93bb92da6a7cf53d0b853c10e56cb31bbdfd0247
stsoon <dragon262@naver.com>
2020-10-23 07:38:55 +0000

    add html ui

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'hello'
+#     return 'hello'
+    return render_template("hello.html")
 
 if __name__ == '__main__':
     app.run()

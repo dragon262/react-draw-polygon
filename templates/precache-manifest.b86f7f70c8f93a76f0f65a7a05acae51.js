e43e5bce56386f9635ef6379f6fdc0743ae2cd89
stsoon <dragon262@naver.com>
2020-10-27 05:14:48 +0000

    react app sample

---
+++ templates/precache-manifest.b86f7f70c8f93a76f0f65a7a05acae51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab2f506aa071ca9704d4b91a5fbd95a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6280d5c65068362813ff",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b6c4a595b28b86f49aed",
+    "url": "/static/js/2.cfc9ec14.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.cfc9ec14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6280d5c65068362813ff",
+    "url": "/static/js/main.8ea0ca49.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

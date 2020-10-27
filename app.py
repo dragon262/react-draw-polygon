12bcaf479405294a4ecfc183fa61a2cb9c4bb3f6
stsoon <dragon262@naver.com>
2020-10-27 05:16:01 +0000

    change index ui template

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 #     return 'hello'
-    return render_template("hello.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()

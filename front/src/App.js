a6ade69204a707b1cdea9073fc9c571eca8b6d09
stsoon <dragon262@naver.com>
2020-10-27 06:16:15 +0000

    add click event

--- front/src/App.js
+++ front/src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const onclick = () => {
-      alert("clicked!")
-  }
+  const onClick = () => {
+      alert("clicked!");
+  };
   
   return (
     <div className="App">
       <h3>sample</h3>
-      <button onclick={onclick}>test</button>
+      <button onClick={onClick}>test</button>
     </div>
   );
 }
